e7bf1dd889f0d1719aa355c7851e10e41af6b413
ponmil3 <samu3loni@gmail.com>
2024-11-12 22:13:18 -0500

    Wk10

---
+++ contact.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const contactSection = document.querySelector(".contact-section");
+  
+    // Add animation class to contact section after page loads
+    setTimeout(() => {
+      contactSection.classList.add("animate");
+    }, 300); // Delay for better visual effect
+  });
+  
